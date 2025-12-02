f40a0b6851c9bf86b4aac378f1249077ad965f54
NicusorN5 <NicusorN5@proton.me>
2025-12-02 23:52:25 +0200

    Rename 3DRadSpace_Input to 3DRadSpace.Input

--- 3DRadSpace/3DRadSpace_CSharp/Input/Keyboard.cs
+++ 3DRadSpace/3DRadSpace_CSharp/Input/Keyboard.cs
@@ -11,16 +11,16 @@ namespace Engine3DRadSpace.Input
             _kb = kb;
         }
 
-        [DllImport("3DRadSpace_Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_IsKeyUp")]
+        [DllImport("3DRadSpace.Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_IsKeyUp")]
         private static extern bool isKeyUp(IntPtr kb, Key k);
 
-        [DllImport("3DRadSpace_Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_IsKeyDown")]
+        [DllImport("3DRadSpace.Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_IsKeyDown")]
         private static extern bool isKeyDown(IntPtr kb, Key k);
 
-        [DllImport("3DRadSpace_Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_IsAnyKeyDown")]
+        [DllImport("3DRadSpace.Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_IsAnyKeyDown")]
         private static extern bool isAny(IntPtr kb);
 
-        [DllImport("3DRadSpace_Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_At")]
+        [DllImport("3DRadSpace.Input.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "E3DRSP_Keyboard_At")]
         private static extern KeyState at(IntPtr kb, Key k);
 
         public bool IsKeyUp(Key k)
