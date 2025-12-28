a0392b92138015498fc736d59dd30e6eeaddf5cf
NicusorN5 <NicusorN5@proton.me>
2025-12-28 07:56:28 +0200

    Fix possible undefined behiavour in E3DRSP_Viewport_Unproject

--- 3DRadSpace/Engine3DRadSpace/Graphics/Viewport.cpp
+++ 3DRadSpace/Engine3DRadSpace/Graphics/Viewport.cpp
@@ -38,11 +38,11 @@ E3DRSP_Vector3 E3DRSP_Viewport_Unproject(
 	const E3DRSP_Matrix4x4* world
 )
 {
-	Viewport vp = *reinterpret_cast<const Viewport*>(viewport);
-	Vector3 src = *reinterpret_cast<const Vector3*>(source);
-	Matrix4x4 proj = *reinterpret_cast<const Matrix4x4*>(projection);
-	Matrix4x4 v = *reinterpret_cast<const Matrix4x4*>(view);
-	Matrix4x4 w = *reinterpret_cast<const Matrix4x4*>(world);
+	Viewport vp = std::bit_cast<const Viewport>(*viewport);
+	Vector3 src = std::bit_cast<const Vector3>(*source);
+	Matrix4x4 proj = std::bit_cast<const Matrix4x4>(*projection);
+	Matrix4x4 v = std::bit_cast<const Matrix4x4>(*view);
+	Matrix4x4 w = std::bit_cast<const Matrix4x4>(*world);
 	
 	Vector3 result = vp.Unproject(src, proj, v, w);
 	return E3DRSP_Vector3{ result.X, result.Y, result.Z };
